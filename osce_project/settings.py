f96122096200b1b6fd9fe0dc2f9f9c8f643e4be6
zulfafalah <zulfafalah@samb.co.id>
2025-08-27 15:42:30 +0700

    Add 'unfold' to INSTALLED_APPS and update requirements.txt to include django-unfold

--- osce_project/settings.py
+++ osce_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost.ums.ac.id','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
